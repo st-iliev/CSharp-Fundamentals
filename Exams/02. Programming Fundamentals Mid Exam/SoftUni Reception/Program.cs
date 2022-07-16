1f10344381282495f679f1a777647c309e3959a4
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-07-16 10:19:28 +0300

    Practice Exam Solution

---
+++ Exams/02. Programming Fundamentals Mid Exam/SoftUni Reception/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02._Programming_Fundamentals_Mid_Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int studentPerHour = num1 + num2 + num3;
+            int students = int.Parse(Console.ReadLine());
+            int hourCount = 0;
+            while (students>0)
+            {
+                hourCount++;
+                if (hourCount % 4 == 0)
+                {
+                    continue;
+                }
+                students -= studentPerHour;
+            }
+            Console.WriteLine($"Time needed: {hourCount:F0}h.");
+
+        }
+    }
+}
---
+++ Exams/06. Programming Fundamentals Mid Exam Retake/Treasure Hunt/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> chest = Console.ReadLine().Split("|").ToList();
+            string[] command = Console.ReadLine().Split(" ").ToArray();
+            List<string> stolenItems = new List<string>();
+            double sum = 0;
+            while (command[0] != "Yohoho!")
+            {
+                if (command[0] == "Loot")
+                {
+                    for (int i = 1; i < command.Length; i++)
+                    {
+                        if (!chest.Contains(command[i]))
+                        {
+                            chest.Insert(0, command[i]);
+                        }
+                    }
+                }
+                else if (command[0] == "Drop")
+                {
+                    int index = int.Parse(command[1]);
+                    string currentLoot = "";
+                    if (index >= 0 && index < chest.Count)
+                    {
+                        currentLoot = chest[index];
+                        chest.RemoveAt(index);
+                        chest.Add(currentLoot);
+                    }
+                }
+                else if (command[0] == "Steal")
+                {
+                    int count = int.Parse(command[1]);
+                    if (count > chest.Count)
+                    {
+                        count = chest.Count;
+                    }
+                    for (int i = 0; i < count; i++)
+                    {
+                        stolenItems.Insert(0, chest[chest.Count - 1]);  
+                        chest.RemoveAt(chest.Count - 1);
+                    }
+                    Console.WriteLine(string.Join(", ",stolenItems));
+                    stolenItems.Clear();
+                }
+                command = Console.ReadLine().Split().ToArray();
+            }
+            foreach (var item in chest)
+            {
+                sum += item.Length;
+            }
+            if (chest.Count == 0)
+            {
+                Console.WriteLine($"Failed treasure hunt.");
+            }
+            else
+            {
+            Console.WriteLine($"Average treasure gain: {sum / chest.Count:F2} pirate credits.");
+            }
+        }
+    }
+}
