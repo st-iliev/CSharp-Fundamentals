22011255c5f60922765780ba04dcda6ef7b48b0e
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-07-16 10:17:36 +0300

    Practice Exam Solution

---
+++ Exams/01. Programming Fundamentals Mid Exam Retake/Computer Store/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _01._Programming_Fundamentals_Mid_Exam_Retake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = "";
+            double sum = 0;
+            double sumWithTaxes = 0;
+            while (true)
+            {
+                type = Console.ReadLine();
+                if (type == "special" || type == "regular")
+                {
+                    break;
+                }
+               double pricePart = double.Parse(type);
+                if (pricePart < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    continue;
+                }
+                sum += pricePart;
+            }
+            sumWithTaxes = sum + (sum * 0.2);
+            if (type == "special")
+            {
+                sumWithTaxes -= sumWithTaxes * 0.1;
+            }
+            if (sum > 0)
+            {
+                Console.WriteLine("Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {sum:F2}$");
+                Console.WriteLine($"Taxes: {sum*0.2:F2}$");
+                Console.WriteLine("-----------");
+                Console.WriteLine($"Total price: {sumWithTaxes:F2}$");
+            }
+            else
+            {
+                Console.WriteLine($"Invalid order!");
+            }
+        }
+    }
+}
---
+++ Exams/01. Programming Fundamentals Mid Exam Retake/Memory Game/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> elements = Console.ReadLine().Split().ToList();
+            List<string> indexes = Console.ReadLine().Split().ToList();
+            int movesCount = 0;
+            while (indexes[0] != "end")
+            {
+                int index1 = int.Parse(indexes[0]);
+                int index2 = int.Parse(indexes[1]);
+                movesCount--;
+                string additionElement = "";
+                if (index1 < 0 || index1 > elements.Count - 1 ||
+                    index2 < 0 || index2 > elements.Count - 1)
+                {
+                    additionElement = movesCount.ToString();
+                    additionElement += "a";
+                    Console.WriteLine($"Invalid input! Adding additional elements to the board");
+                    elements.Insert(elements.Count / 2, additionElement);
+                    elements.Insert(elements.Count / 2+1, additionElement);
+                }
+                else if (elements[index1] == elements[index2])
+                {
+                    Console.WriteLine($"Congrats! You have found matching elements - {elements[index1]}!");
+                    if (index1 > index2)
+                    {
+                        elements.RemoveAt(index1);
+                        elements.RemoveAt(index2);
+                    }
+                    else
+                    {
+                        elements.RemoveAt(index2);
+                        elements.RemoveAt(index1);
+                    }
+                }  
+                else if (elements[index1] != elements[index2])
+                {
+                    Console.WriteLine($"Try again!");
+                }
+                if (elements.Count == 0)
+                {
+                    Console.WriteLine($"You have won in {Math.Abs(movesCount)} turns!");
+                    break;
+                }
+                    indexes = Console.ReadLine().Split().ToList();
+            }
+            if (elements.Count > 0)
+            {
+                Console.WriteLine($"Sorry you lose :(");
+                Console.WriteLine(string.Join(" ",elements));
+            }
+
+        }
+    }
+}
